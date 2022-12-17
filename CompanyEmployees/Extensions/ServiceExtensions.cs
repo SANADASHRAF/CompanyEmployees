7a0b4518594f18ab0e36c632a9c45a573d06195f
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-12-17 16:32:20 +0200

    program configration ,CORS and IIS integrated

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -2,13 +2,20 @@
 {
    public static class ServiceExtensions
     {
+        
         public static void ConfigureCors(this IServiceCollection services) =>
          services.AddCors(options =>
          {
              options.AddPolicy("CorsPolicy", builder =>
-             builder.AllowAnyOrigin()
-             .AllowAnyMethod()
-             .AllowAnyHeader());
+             builder.AllowAnyOrigin() //withOrigin()
+             .AllowAnyMethod()//WithMethods()
+             .AllowAnyHeader());//WithHeaders()
+         });
+
+       public static void ConfigureIISIntegration(this IServiceCollection services) =>
+         services.Configure<IISOptions>(options =>
+         {
+
          });
 
     }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,13 +1,36 @@
+// builder is amember of type class webapplicationbuilder that responsaple for many thing such(configration and service)
 
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+        // registers only the controllers in IServiceCollection
 builder.Services.AddControllers();
+        //method in extention
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+         //HTTP Strict Transport Security "Protect websites from attacks by phishing attackers"
+    app.UseHsts();
+app.UseHttpsRedirection();
+        //enables using static files for the request
+app.UseStaticFiles();
+        //help us during application deployment and authontication.
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
 // Configure the HTTP request pipeline.
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
