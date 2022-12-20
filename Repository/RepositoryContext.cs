d8e61fc4d5bcefd3513c86d1a74e0e92c6db4cbe
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-12-20 18:05:43 +0200

    add miggration and initial data for models

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,12 @@ namespace Repository
     {
         public RepositoryContext(DbContextOptions options):base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
         }
         public DbSet<Company>? Companies { get; set; }
         public DbSet<Employee>? Employees { get; set; }
